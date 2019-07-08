1e3b2ad28b9e0ba8245ff3acbdfc81463c073be2
unknown <sallamtanna2015@hotmail.com>
2019-07-08 17:09:36 +0300

    Add: NearBy screen

--- screens/NearByScreen.js
+++ screens/NearByScreen.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, View, Text, Image, Alert } from 'react-native';
+import CustomButton from '../components/CustomButton.js';
+import Layout from '../constants/Layout'
 
 export default function NearByScreen() {
   return (
-    <ScrollView style={styles.container}>
-
-    </ScrollView>
+      <ScrollView style={styles.container}>
+        <Text style={styles.header}>find the nearest shop!</Text>
+        <Image source={require('../assets/images/gps.png')} />
+        <Text style={styles.paragraph}>It is a long established fact that a reader will be distracted by the readablli</Text>
+        <CustomButton onPress={() => Alert.alert('Hi')} text="location" textStyle={styles.textStyle} backgroundColor="#8A04B2"  height={46}/>
+      </ScrollView>
   );
 }
 
@@ -13,10 +18,40 @@ NearByScreen.navigationOptions = {
   header: null,
 };
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
     backgroundColor: '#fff',
+    margin:  Layout.screenMargin,
+    textAlign: 'center',
+    color: '#000',
+    fontFamily: 'Poppins',
   },
+  header: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+    textTransform: 'capitalize',
+    marginTop: 94,
+    marginLeft: 33,
+    marginRight: 34,
+    marginBottom: 40,
+  },
+  paragraph: {
+    textAlign: 'center',
+    fontSize: 14,
+    opacity: .44,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 30,
+    marginBottom: 56.7,
+  },
+  textStyle:{
+    color: "#fff",
+    textAlign: 'center',
+    fontSize: 13,
+    fontFamily: 'Poppins',
+    textTransform: 'uppercase',
+  }
 });
