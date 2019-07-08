c831b174c10318482ae695c8d85984fb3523b14b
MohammedYehia <m-ya@windowslive.com>
2019-07-08 17:34:36 +0300

    Add: add CustomeIcon component

---
+++ components/CustomIcon.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Platform, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+export default function CustomIcon(props) {
+    // return <Icon name={props.name} color={props.color} size={30}/>
+    return(
+        <TouchableOpacity onPress={props.onPress}>
+            <Ionicons
+                name={
+                    Platform.OS === 'ios'
+                        ? `ios-${props.name}`
+                        : `md-${props.name}`
+                }
+                color={props.active ? props.color : '#d1d1d1'}
+                size={30}
+            />
+        </TouchableOpacity>
+    )
+}
