6104b4185691b5378c302a40b259bfb8ada5e316
asmaa-ezz <asmadhoun@gmail.com>
2019-07-27 16:02:26 +0300

    Edit: edit DATACATEGORY fragmentation for two parts

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
     StyleSheet,
     Text,
     ImageBackground,
     View,
 } from 'react-native';
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import HotDealsCarousel from '../components/Carousel/HotDealsCarousel';
 import Layout from '../constants/Layout'
 import Colors from "../constants/Colors";
@@ -13,23 +13,26 @@ import ButtonIcon from '../components/CategoryButton/ButtonIcon'
 import CustomIcon from '../components/CustomIcon';
 
 import DataActions from '../redux/MyDataRedux'
-import {Notifications} from 'expo';
-import {registerForPushNotificationsAsync} from '../services/registerForPushNotificationsAsync'
+import { Notifications } from 'expo';
+import { registerForPushNotificationsAsync } from '../services/registerForPushNotificationsAsync'
 
-const DATACATEGORY = [
-    {id: 1, text: 'Bars & Pub', color: '#6F4EF2', icon: 'beer'},
-    {id: 2, text: 'Clothing', color: '#4EF279', icon: 'shirt'},
-    {id: 3, text: 'Salon & Spa', color: '#9D3EF1', icon: 'cut'},
-    {id: 4, text: 'Food', color: '#EF0097', icon: 'pizza'},
-    {id: 5, text: 'Health', color: '#FD6300', icon: 'heart'},
-    {id: 6, text: 'Gym & Yoga', color: '#43B8FF', icon: 'fitness'},
-    {id: 7, text: 'Accessories', color: '#2AD7EA', icon: 'walk'},
-    {id: 8, text: 'Hotels', color: '#F5D142', icon: 'bed'},
+const DATACATEGORY1 = [
+    { id: 1, text: 'Bars & Pub', color: '#6F4EF2', icon: 'beer' },
+    { id: 2, text: 'Clothing', color: '#4EF279', icon: 'shirt' },
+    { id: 3, text: 'Salon & Spa', color: '#9D3EF1', icon: 'cut' },
+    { id: 4, text: 'Food', color: '#EF0097', icon: 'pizza' },
+];
+
+const DATACATEGORY2 = [
+    { id: 5, text: 'Health', color: '#FD6300', icon: 'heart' },
+    { id: 6, text: 'Gym & Yoga', color: '#43B8FF', icon: 'fitness' },
+    { id: 7, text: 'Accessories', color: '#2AD7EA', icon: 'walk' },
+    { id: 8, text: 'Hotels', color: '#F5D142', icon: 'bed' },
 ];
 
 class HomeScreen extends React.Component {
 
-    static navigationOptions = ({navigation}) => {
+    static navigationOptions = ({ navigation }) => {
         return {
             header: null,
         };
@@ -52,7 +55,7 @@ class HomeScreen extends React.Component {
     }
 
     _handleNotification = (notification) => {
-        this.setState({notification: notification});
+        this.setState({ notification: notification });
     };
 
     handleButtonIcon(id) {
@@ -66,10 +69,10 @@ class HomeScreen extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <ImageBackground source={require('../assets/images/header-bg.png')} style={{height: '60%'}}>
+                <ImageBackground source={require('../assets/images/header-bg.png')} style={{ height: '60%' }}>
                     <View style={styles.customHeader}>
                         <Text style={styles.titleHeader}>VOV DEALS</Text>
-                        <CustomIcon name='search' size={24}/>
+                        <CustomIcon name='search' size={24} />
                     </View>
                     <HotDealsCarousel></HotDealsCarousel>
                 </ImageBackground>
@@ -82,9 +85,20 @@ class HomeScreen extends React.Component {
                         paddingTop: 10,
                         flexWrap: 'wrap'
                     }}>
-                        {DATACATEGORY.map(item => {
+                        {DATACATEGORY1.map(item => {
                             return <ButtonIcon _onPressButton={() => this.handleButtonIcon(item.id)} key={item.id}
-                                               color={item.color} icon={item.icon} text={item.text}/>
+                                color={item.color} icon={item.icon} text={item.text} />
+                        })}
+                    </View>
+                    <View style={{
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        paddingTop: 10,
+                        flexWrap: 'wrap'
+                    }}>
+                        {DATACATEGORY2.map(item => {
+                            return <ButtonIcon _onPressButton={() => this.handleButtonIcon(item.id)} key={item.id}
+                                color={item.color} icon={item.icon} text={item.text} />
                         })}
                     </View>
                 </View>
